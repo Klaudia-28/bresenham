3ab93b7840f10e94b7fb195e34868187fce98eb9
Klaudia Perez <est.klaudia.perez@unimilitar.edu.co>
2026-04-03 10:23:30 -0500

    se obtienen las coordenadas y dibuja puntos inicial y final en canvas

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
 }
 
+//funcion principal
+function dibujarLinea(){
+
+    //obtener valores
+    const x0 = parseInt(document.getElementById("x0").value);
+    const y0 = parseInt(document.getElementById("y0").value);
+    const x1 = parseInt(document.getElementById("x1").value);
+    const y1 = parseInt(document.getElementById("y1").value);
+
+    limpiarCanvas();
+
+    //dibuja puntos inicial y final
+    ctx.fillRect(x0, y0, 4, 4);
+    ctx.fillRect(x1, y1, 4, 4);
+}
